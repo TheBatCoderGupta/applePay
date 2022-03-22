93d88acf0e5b0ba972948fd8397df24f9e7e6bcc
TheBatCoderGupta <61503574+TheBatCoderGupta@users.noreply.github.com>
2022-03-22 16:09:41 +0530

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -104,7 +104,7 @@ function updateSubscriptionOnServer(subscription) {
         };
 
         $.ajax({
-            url: 'http://azudevapt200004.aptifydev.com/SOA/v1/addPushSubscription',
+            url: 'https://azudevapt200004.aptifydev.com/SOA/v1/addPushSubscription',
             type: "POST",
             data: dataToSend,
             success: function () {
